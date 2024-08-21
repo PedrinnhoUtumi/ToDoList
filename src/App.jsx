b6a8733a877d768efae2c41823a8c47ecd834589
Pedro Felipe Onofre Utumi <130059527+PedrinnhoUtumi@users.noreply.github.com>
2024-08-21 11:00:53 -0300

    react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+export function App() {
+  return (
+    <>
+    <h1>AAAAA</h1>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur aliquid delectus odio quos, ullam iste libero, aut soluta similique corrupti non voluptate earum ipsam nesciunt incidunt cupiditate aliquam reprehenderit distinctio.</div>
+    </>
+  )
+}
+
+ 
